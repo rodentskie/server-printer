4dc4678f4b2283763036040c71c70f92c7ac6669
rodentskie <rodentskie@whatever.com>
2020-02-24 15:26:43 +0800

    find printer fix

--- src/use-case/fsqr-qr/find-printer.js
+++ src/use-case/fsqr-qr/find-printer.js
@@ -1,6 +1,7 @@
 const findPrinter = ({ escpos }) => {
   return async function selects(info) {
-    return escpos.USB.findPrinter();
+    // return escpos.USB.findPrinter();
+    return escpos.USB;
   };
 };
 
